e8198f2c49220b7819766bd37c1bf84db9c73d25
jdelles <james.delles@gmail.com>
2021-07-16 23:02:14 -0400

    Game board displaying

--- app.js
+++ app.js
@@ -21,10 +21,19 @@ const gameController = (() => {
     
 })(); 
 
-const Player = (name) => {
+const Player = (name, token) => {
     let score = 0; 
     const getName = () => name; 
+    const getToken = () => token; 
     const getScore = () => score; 
     const incrementScore = () => score++; 
-    return {getName, getScore, incrementScore}; 
+    return {getName, getToken, getScore, incrementScore}; 
 }
+
+const intializeGame = (() => {
+    const player1name = alert("Enter player 1's name:"); 
+    const player1 = Player(player1name, "X"); 
+    const player2name = alert("Enter player 2 name:"); 
+    const player2 = Player(player2name, "X"); 
+    return {player1, player2}; 
+})(); 
