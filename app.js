1d40b1dd25e4a5ae366836fdc0bb486e6d0f2adc
jdelles <james.delles@gmail.com>
2021-07-20 23:47:14 -0400

    Working! Tie game is not reporting yet. Need to upgrade the GUI

--- app.js
+++ app.js
@@ -1,35 +1,61 @@
 /**
- * Builds the game board 
+ * Builds the game board and provides functionality to set the player token
+ * on the board. 
  */
 const gameboard = (() => {
-    const board = [];  
-    for (let i = 0; i < 9; i++) {
-        board.push(" "); 
-    } 
+    let board = new Array(9);  
     let turn = 0; 
-    return {board, turn}; 
+
+    const getPosition = (index) => board[index]; 
+
+    const setPosition = (index, token) => {
+        const button = document.querySelector(`.board[data-id="${index}"]`); 
+        button.textContent = token; 
+        board[index] = token; 
+        console.log(board[index]); 
+        incrementTurn(); 
+    }
+
+    const newGame = () => {
+        const buttons = document.querySelectorAll('.board'); 
+        buttons.forEach(button => {
+            button.textContent = ""; 
+        });
+        turn = 0; 
+    }
+
+    const incrementTurn = () => turn++; 
+
+    const getTurn = () => turn; 
+
+    return {setPosition, getPosition, incrementTurn, getTurn, newGame}; 
 })(); 
 
-/**
- * Player objects created with factory design pattern
- * @param {*} name the player's name
- * @param {*} token the player's game token
- * @param {*} score the player's score
- * @returns name, token and score
- */
- const Player = (name, token, score) => {
-    return {name, token, score}
-}
+
 
 /**
  * Sets up the two players
  * Currently uses JavaScript prompt() would like to eventually use a form
  */
-const setupPlayers = (() => {
+const Players = (() => {
+
+    /**
+     * Player objects created with factory design pattern
+     * @param {*} name the player's name
+     * @param {*} token the player's game token
+     * @param {*} score the player's score
+     * @returns name, token and score
+     */
+    const Player = (name, token, score) => {
+        return {name, token, score}
+    }
+
     const player1name = prompt("Enter player 1's name:"); 
     const player1 = Player(player1name, "X", 0); 
+    
     const player2name = prompt("Enter player 2 name:"); 
     const player2 = Player(player2name, "O", 0); 
+    
     return {player1, player2}; 
 })(); 
 
@@ -39,69 +65,80 @@ const setupPlayers = (() => {
 const gameController = (() => {
     const getCurrentPlayer = (turn) => {
         if (turn % 2 === 0) {
-            return setupPlayers.player1; 
+            return Players.player1; 
         } else {
-            return setupPlayers.player2; 
+            return Players.player2; 
         }
 
     }
     const isWinner = () => {
-        const board = Array.from(document.querySelectorAll(".board"));
-        let boardButtons = []; 
-        for (let i = 0; i < 9; i++) {
-            boardButtons.push(board[i].textContent); 
-        }
-        if ((boardButtons[0] === boardButtons[1] && boardButtons[0] === boardButtons[2] && boardButtons[0] != " ") ||
-            (boardButtons[0] === boardButtons[3] && boardButtons[0] === boardButtons[6] && boardButtons[0] != " ") ||
-            (boardButtons[0] === boardButtons[4] && boardButtons[0] === boardButtons[8] && boardButtons[4] != " ") ||
-            (boardButtons[1] === boardButtons[5] && boardButtons[1] === boardButtons[7] && boardButtons[4] != " ") ||
-            (boardButtons[2] === boardButtons[4] && boardButtons[2] === boardButtons[6] && boardButtons[4] != " ") ||
-            (boardButtons[3] === boardButtons[4] && boardButtons[3] === boardButtons[5] && boardButtons[4] != " ") ||
-            (boardButtons[2] === boardButtons[5] && boardButtons[2] === boardButtons[8] && boardButtons[8] != " ") ||
-            (boardButtons[6] === boardButtons[7] && boardButtons[6] === boardButtons[8] && boardButtons[8] != " ")) {
+        //  0 1 2
+        //  3 4 5
+        //  6 7 8
+        if ((gameboard.getPosition(0) === gameboard.getPosition(1) && gameboard.getPosition(0) === gameboard.getPosition(2) && gameboard.getPosition(0) != undefined) ||
+            (gameboard.getPosition(0) === gameboard.getPosition(3) && gameboard.getPosition(0) === gameboard.getPosition(6) && gameboard.getPosition(0) != undefined) ||
+            (gameboard.getPosition(0) === gameboard.getPosition(4) && gameboard.getPosition(0) === gameboard.getPosition(8) && gameboard.getPosition(4) != undefined) ||
+            (gameboard.getPosition(1) === gameboard.getPosition(4) && gameboard.getPosition(1) === gameboard.getPosition(7) && gameboard.getPosition(4) != undefined) ||
+            (gameboard.getPosition(2) === gameboard.getPosition(4) && gameboard.getPosition(2) === gameboard.getPosition(6) && gameboard.getPosition(4) != undefined) ||
+            (gameboard.getPosition(3) === gameboard.getPosition(4) && gameboard.getPosition(3) === gameboard.getPosition(5) && gameboard.getPosition(4) != undefined) ||
+            (gameboard.getPosition(2) === gameboard.getPosition(5) && gameboard.getPosition(2) === gameboard.getPosition(8) && gameboard.getPosition(8) != undefined) ||
+            (gameboard.getPosition(6) === gameboard.getPosition(7) && gameboard.getPosition(6) === gameboard.getPosition(8) && gameboard.getPosition(8) != undefined)) {
                 return true; 
             } else {
                 return false; 
             }
     }
+
+    const isTie = (turn) => {
+        if (!isWinner && turn >= 8) {
+            return true; 
+        } else {
+            return false; 
+        }
+    }
+
+    const addListeners = () => {
+        const boardButtons = Array.from(document.querySelectorAll(".board"));
+        let id = 0; 
+        boardButtons.forEach(button => {
+            button.setAttribute("data-id", id);
+            button.addEventListener('click', () => {
+                if (button.textContent === "X" || button.textContent === "O") {
+                    alert("This space is already taken. Choose another."); 
+                } else {
+                    const playToken = gameController.getCurrentPlayer(gameboard.getTurn()).token; 
+                    gameboard.setPosition(button.getAttribute("data-id"), playToken);  
+                    if (gameController.isWinner()) {
+                        console.log(playToken + " wins!"); 
+                    } else if (isTie(gameboard.getTurn())) {
+                        console.log("It's a tie!"); 
+                    }
+                }
+            }); 
+            id++; 
+        });
+    }
+    addListeners(); 
     return {getCurrentPlayer, isWinner}
 })(); 
 
 
 
-const boardButtons = Array.from(document.querySelectorAll(".board"));
-let i = 0; 
-boardButtons.forEach(button => {
-    button.setAttribute("data-id", i);
-    button.addEventListener('click', () => {
-        if (button.textContent === "X" || button.textContent === "O") {
-            alert("This space is already taken. Choose another."); 
-        } else {
-            const playToken = gameController.getCurrentPlayer(gameboard.turn).token; 
-            button.textContent = playToken; 
-            gameboard.board[i] = playToken; 
-            gameboard.turn++; 
-            if (gameboard.turn >= 5 && gameController.isWinner()) {
-                console.log(playToken + " wins!"); 
-            }
-        }
-    }); 
-    i++; 
-});
 
 /**
  * Essentials /////////////////
  * 
- * Draw a board
- * Build functionality for user to interact with board
- * Store the X or O 
- * Check for win conditions / tie
+ * DONE - Draw a board
+ * DONE - Build functionality for user to interact with board
+ * DONE - Store the X or O 
+ * DONE - Check for win conditions / tie
  * Report back win conditions if met and end game
  * Report back tie if game ends with no win condition
  * Reset game
  * 
  * Additional //////////////////
- * Store users name and use it in win reporting and keep track of games won
+ * Switch from Prompt to form - Store users name and use it in win reporting and keep track of games won
  * Build "AI" to play game randomly
  * Build smarter AI with min-max algorithm to never lose
+ * Add score to keep track of how many games each player has won
  */
